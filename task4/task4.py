ab8663cb87156495181645616b75d8bf51b2f6c2
FreakyOwl <69248671+FreakyOwl@users.noreply.github.com>
2024-09-11 19:09:42 +0300

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,8 +1,10 @@
 import statistics
 import random
+import sys
+import os
 
 
-def task4 (path):
+def task4(path):
     with open(path, "r") as f1:
         numbers = [int(line) for line in f1]
         med = round(statistics.median(numbers))
@@ -10,12 +12,16 @@ def task4 (path):
     print(turns)
 
 
-path = "numbers.txt"
+if sys.argv[1:]:
+    args = sys.argv[1:]
+    path = args[0]
+else:
+    path = "numbers.txt"
 
-num_num = random.randrange(1, 11)
-numbers = [f"{str(random.randrange(-10, 11))}\n" for i in range(num_num)]
-
-with open(path, "w") as f1:
-    f1.writelines(numbers)
+if not os.path.isfile(path):
+    num_num = random.randrange(1, 11)
+    numbers = [f"{str(random.randrange(-10, 11))}\n" for i in range(num_num)]
+    with open(path, "w") as f1:
+        f1.writelines(numbers)
 
 task4(path)
